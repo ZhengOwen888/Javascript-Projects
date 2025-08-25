fe9b272d38814245c9921de0f39c4763e6a15bd4
Owen Zheng <owenzheng922@gmail.com>
2025-08-25 16:25:07 -0400

    Add files via upload

---
+++ Modal Window/script.js
@@ -0,0 +1,33 @@
+"use strict";
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+console.log(btnsOpenModal);
+
+const openModal = function () {
+    console.log("Button clicked");
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+};
+
+// Open modal
+for (let i = 0; i < btnsOpenModal.length; i++) {
+    btnsOpenModal[i].addEventListener("click", openModal);
+}
+// Close modal
+btnCloseModal.addEventListener("click", closeModal); // closeModal() is incorrect because it will call the function immediately
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (event) {
+    console.log(event.key);
+    if (event.key === "Escape" && !modal.classList.contains("hidden")) {
+        closeModal();
+    }
+});
