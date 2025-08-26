2b2c815bf30d28422839fa8e265ae4d71d30ab54
Owen Zheng <owenzheng922@gmail.com>
2025-08-25 20:15:21 -0400

    Update script.js

--- Modal Window/script.js
+++ Modal Window/script.js
@@ -1,10 +1,10 @@
 "use strict";
 
+// Get property 
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnsOpenModal = document.querySelectorAll(".show-modal");
-console.log(btnsOpenModal);
 
 const openModal = function () {
     console.log("Button clicked");
